c640454ed11d2fbb1418438a710dc7293d412a22
greyovo <grey030@foxmail.com>
2024-02-09 01:02:18 +0800

    feat: shadcn-ui的对话框

--- src-tauri/src/fs_api.rs
+++ src-tauri/src/fs_api.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
 #[tauri::command]
-pub fn rename_dir(oldPath: &str, newPath: &str) {
-    let _ = fs::rename(oldPath, newPath);
+pub fn rename_dir(old_path: &str,new_path: &str) {
+    let _ = fs::rename(old_path, new_path);
 }
--- src/feat/directory_panel/DirectoryContextMenu.tsx
+++ src/feat/directory_panel/DirectoryContextMenu.tsx
@@ -1,43 +1,49 @@
 import { useDialog } from "@/components/dialog/Dialog";
 import { DialogContext } from "@/components/dialog/DialogContext";
+import { Dialog, DialogHeader } from "@/components/ui/dialog";
 import { deleteDirectory, deleteFile, openFile, renameDirectory, renameFile } from "@/store/directory";
 import { createNewDoc } from "@/store/document";
-import { ContextMenu } from "@radix-ui/themes";
+import { ContextMenu, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "@radix-ui/themes";
+import { useState } from "react";
 
 export function DirectoryContextMenu({ children, entity, onRename }: { children: React.ReactNode, entity: DirectoryEntity, onRename: () => void }) {
   const { openDialog } = useDialog();
 
   function handleDelete() {
-    openDialog({
-      title: `删除: ${entity.name}`,
-      content: "确定？",
-      danger: true,
-      confirmText: "删除",
-      denyText: "取消",
-      onConfirm: () => {
-        if (entity.type === 'file') {
-          deleteFile(entity)
-        } else {
-          deleteDirectory(entity)
-        }
-      }
-    })
+    setOpen(true)
+    // openDialog({
+    //   title: `删除: ${entity.name}`,
+    //   content: "确定？",
+    //   danger: true,
+    //   confirmText: "删除",
+    //   denyText: "取消",
+    //   onConfirm: () => {
+    //     if (entity.type === 'file') {
+    //       deleteFile(entity)
+    //     } else {
+    //       deleteDirectory(entity)
+    //     }
+    //   }
+    // })
   }
 
-  return (
-    <ContextMenu.Root>
-      <ContextMenu.Trigger>
-        {children}
-      </ContextMenu.Trigger>
-      <ContextMenu.Content>
-        <ContextMenu.Item onClick={() => openFile(entity.path)}>打开</ContextMenu.Item>
-        <ContextMenu.Item onClick={onRename}>重命名</ContextMenu.Item>
-        <ContextMenu.Item shortcut="⌘ D">创建副本</ContextMenu.Item>
-        <ContextMenu.Separator />
-        <ContextMenu.Item onClick={() => createNewDoc()}>新建文件</ContextMenu.Item>
-        <ContextMenu.Item shortcut="⌘ N">新建文件夹</ContextMenu.Item>
+  const [open, setOpen] = useState(false)
 
-        {/* <ContextMenu.Sub>
+  return (
+    <>
+      <ContextMenu.Root>
+        <ContextMenu.Trigger>
+          {children}
+        </ContextMenu.Trigger>
+        <ContextMenu.Content>
+          <ContextMenu.Item onClick={() => openFile(entity.path)}>打开</ContextMenu.Item>
+          <ContextMenu.Item onClick={onRename}>重命名</ContextMenu.Item>
+          <ContextMenu.Item shortcut="⌘ D">创建副本</ContextMenu.Item>
+          <ContextMenu.Separator />
+          <ContextMenu.Item onClick={() => createNewDoc()}>新建文件</ContextMenu.Item>
+          <ContextMenu.Item shortcut="⌘ N">新建文件夹</ContextMenu.Item>
+
+          {/* <ContextMenu.Sub>
           <ContextMenu.SubTrigger>More</ContextMenu.SubTrigger>
           <ContextMenu.SubContent>
             <ContextMenu.Item>Move to project…</ContextMenu.Item>
@@ -47,11 +53,25 @@ export function DirectoryContextMenu({ children, entity, onRename }: { children:
           </ContextMenu.SubContent>
         </ContextMenu.Sub> */}
 
-        {/* <ContextMenu.Item>Share</ContextMenu.Item>
+          {/* <ContextMenu.Item>Share</ContextMenu.Item>
         <ContextMenu.Item>Add to favorites</ContextMenu.Item> */}
-        <ContextMenu.Separator />
-        <ContextMenu.Item color="red" onClick={handleDelete}>删除</ContextMenu.Item>
-      </ContextMenu.Content>
-    </ContextMenu.Root>
+          <ContextMenu.Separator />
+          <ContextMenu.Item color="red" onClick={handleDelete}>删除</ContextMenu.Item>
+        </ContextMenu.Content>
+      </ContextMenu.Root>
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
+    </>
   )
 }

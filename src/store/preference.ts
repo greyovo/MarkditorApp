6e781f8e253c4cbde9408dd1e2d77eafb4e0988f
greyovo <grey030@foxmail.com>
2024-02-12 17:46:45 +0800

    feat: 持久化设置信息

--- src/store/preference.ts
+++ src/store/preference.ts
@@ -1,25 +1,38 @@
 import { create } from 'zustand'
-import { getVditor } from './editor'
+import { persist, createJSONStorage } from 'zustand/middleware'
+import { getVditor, syncEditorTheme } from './editor'
 
 interface PreferenceState {
   prefThemeMode: "light" | "dark" | "system", // Default to "system"
   autoSaveTimeout: number, // Default to 8000 ms
+  fileHistory: string[], // Path to file
+  folderHistory: string[], // Path to folder
+
   // Computed
   themeMode: () => "light" | "dark",
 }
 
-const usePreferenceStore = create<PreferenceState>(
-  (set, get) => ({
-    prefThemeMode: "system",
-    autoSaveTimeout: 8000,
+const usePreferenceStore = create(
+  persist<PreferenceState>(
+    (set, get) => ({
+      prefThemeMode: "system",
+      autoSaveTimeout: 8000,
+      fileHistory: [],
+      folderHistory: [],
 
-    themeMode() {
-      if (get().prefThemeMode === "system") {
-        return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+      themeMode() {
+        if (get().prefThemeMode === "system") {
+          return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+        }
+        return get().prefThemeMode === "light" ? "light" : "dark"
       }
-      return get().prefThemeMode === "light" ? "light" : "dark"
-    }
-  })
+    }),
+
+    {
+      name: 'markditor-pref-storage', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+    },
+  )
 )
 
 const { setState, getState, subscribe } = usePreferenceStore
@@ -28,8 +41,7 @@ const { setState, getState, subscribe } = usePreferenceStore
 export function setThemeMode(prefThemeMode: "light" | "dark" | "system") {
   setState((state) => ({ ...state, prefThemeMode }))
   const realThemeMode = getState().themeMode()
-  const editorTheme = realThemeMode === "light" ? "classic" : "dark"
-  getVditor()?.setTheme(editorTheme, realThemeMode)
+  syncEditorTheme()
 }
 
 
